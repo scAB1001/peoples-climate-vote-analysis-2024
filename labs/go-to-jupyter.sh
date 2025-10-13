440895a697a28dbbf69bde70d7d6e91653e4b036
Andreas Bazigos <sc222ab@leeds.ac.uk>
2025-10-13 19:11:27 +0100

    feat: finished 1, started 2

---
+++ labs/go-to-jupyter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Nav to Data Science dir
+cd ./code/comp5122m/
+
+# Ensure compatible version in use
+pyenv local 3.12.12
+
+# Start shell which has all the packages
+poetry shell
+
+# Start notebook to access jupyter notebook(s)
+jupyter-notebook
+
